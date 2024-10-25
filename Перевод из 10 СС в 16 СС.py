f9dd87fdb2795bf2a0ed15184cea60d2574573da
Bander0n <stepanov.yaroslav.10@gmail.com>
2024-10-25 22:11:56 +0500

    ДЗ Степанова Ярослава

---
+++ Перевод из 10 СС в 16 СС.py
@@ -0,0 +1,31 @@
+a=int(input())
+divide=a%16
+max1=0
+lst[]
+while divide>=15:
+    max1=a%16
+    if max1=10:
+        max1='A'
+    elif max1=11:
+        max1='B'
+    elif max1=12:
+        max1='C'
+    elif max1=13:
+        max1='D'
+    elif max1=14:
+        max1='E'
+    elif max1=15:
+        max1='F'
+    lst.append(max1)
+lst.reverse()
+print(lst)
+
+
+
+
+
+
+
+
+
+
