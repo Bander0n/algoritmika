ec3fae461b32b5e65377213e17c596f1a3c77bf5
Bander0n <stepanov.yaroslav.10@gmail.com>
2024-10-30 15:41:52 +0500

    Add files via upload

---
+++ Морской  бой.py
@@ -0,0 +1,21 @@
+mas2 = [[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0]]
+print("  A B C D E F G H")
+for i in range(len(mas2)):
+    print(i+1, end=' ')
+    for j in range(len(mas2[i])):
+        print(mas2[i][j], end=' ')
+    print()
+
+lst=["A","B","C","D","E","F","G","H"]
+print("Игрок 1")
+location1=str(input("введите расположение первого корабля"))
+fori in range( 
+location2=str(input("введите расположение второго корабля"))
+
+location3=str(input("введите расположение третьего корабля"))
+
+location4=str(input("введите расположение четвертого корабля"))
+
+#for i in range(4):
+#    for j in range(4):
+#        mas2.append(location1,1)
